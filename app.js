b69397492e7699305de8329be9295fd93ef1ef7c
nikoladim123 <nikoladimitrijevic1991@gmail.com>
2018-12-19 20:51:23 +0100

    JS anim

---
+++ app.js
@@ -0,0 +1,22 @@
+var AboutMeWhiteAnimDiv = document.getElementsByClassName('AboutMeWhiteAnimDiv')[0];
+var AboutMeBlackAnimDiv = document.getElementsByClassName('AboutMeBlackAnimDiv')[0];
+var aboutMe = document.getElementsByClassName('aboutMe')[0];
+
+var i=0;
+window.addEventListener("scroll", function(evt) {
+  if((window.innerWidth /100)* 86.7  < window.scrollY + window.innerHeight && i!==1){
+    i=1;
+    console.log(window.innerWidth /100 *120);
+    console.log(window.scrollY);
+    AboutMeWhiteAnimDiv.className += ' animateWhiteClass';
+    myFunction()
+    console.log('scroled');
+  }
+});
+
+function myFunction() {
+  setTimeout(function(){
+      aboutMe.style.visibility = 'visible';
+      AboutMeBlackAnimDiv.className += ' animateBlackClass';;
+   }, 440);
+}
